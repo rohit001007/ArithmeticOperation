e2288dff38720f515f451fb7ff0666d58d77c458
rohit001007 <rohit001007@gmail.com>
2020-11-24 15:00:53 +0530

    Done : Operation a+b*c

--- Sort.sh
+++ Sort.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+echo -ne "\n"
+
 read -p "Enter First Number : " a
 read -p "Enter Second Number : " b
 read -p "Enter Third Number : " c
 
-echo "The Numbers Are $a $b $c"
+echo -ne "\n The Numbers Are $a $b $c \n"
+
+p=$(( a+(b*c) ))
+
+echo -ne "\n The Result is $p \n"
